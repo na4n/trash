bff43c073bd8154269c1f5ac5017914a28437b9e
Nathan Annareddy <nathan.annareddy@gmail.com>
2024-09-06 10:06:14 -0500

    update todo in trash.c

--- trash.c
+++ trash.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+int init(){
+    // create init file
+    //      location of trash
+    //      length of time     
+
+    return 0;
+}   
+
+int remove(){
+    // check permission
+    // execute command
+    // return value
+    
+    return 0;
+}
+
 int main(int argc, char** argv){
     if(argc <= 1){
         printf("trash: missing operand[s]\n");
